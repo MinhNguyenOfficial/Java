9c6aa2ead66c874ff6729fb80fecffb1cab288c2
Minh Nguyen <minhnguyentuanofficial@gmail.com>
2023-02-15 18:40:35 +0700

    first commit

---
+++ NguyenTuanMinh_Project/src/NguyenTuanMinh_Packages/NguyenTuanMinh_App.java
@@ -0,0 +1,11 @@
+package NguyenTuanMinh_Packages;
+
+public class NguyenTuanMinh_App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nguyễn Tuấn Minh học Java");
+
+	}
+
+}
